b542b2b97bfb5efa16a5a092465700821b78cdb0
huangtianyou <1156388927@qq.com>
2022-10-03 16:35:04 +0800

    项目骨架搭建完成

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,10 +1,21 @@
 package com.kob.backend.controller.pk;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @RequestMapping("/pk")
 public class IndexController {
 
+    @RequestMapping("getbotinfo")
+    public Map<String,String> getBotInfo(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","tiger");
+        map.put("rating","1500");
+        return map;
+    }
 }
